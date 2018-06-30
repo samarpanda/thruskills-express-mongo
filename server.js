be3e91d147e0c840174ef5ab37e347087a85b04e
Samar Panda <er.samarpanda@gmail.com>
2018-06-30 06:39:15 +0530

    get / & /data responded

--- server.js
+++ server.js
@@ -4,4 +4,26 @@
  * respond jsonData on a get request to '/data'
  */
 
- var jsonData = {count: 12, message: 'Hello World'}
+var jsonData = {count: 12, message: 'Hello World'}
+
+var express = require('express')
+var app = express()
+
+// GET / with index.html
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html', function(err){
+		if(err){
+			res.status(500).send(err)
+		}
+	})
+})
+
+// GET /data with jsonData
+app.get('/data', function(req, res){
+	res.json(jsonData)
+})
+
+var port = 3000
+app.listen(port, function(){
+	console.log('listening on http://localhost', port)
+})
