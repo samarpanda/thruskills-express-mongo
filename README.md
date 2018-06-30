bfbbc7ae2bf35869eba1939092dfe520324e4f11
Samar Panda <er.samarpanda@gmail.com>
2018-06-30 06:21:10 +0530

    workshop lesson 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## Workshop nodejs, express & mongo
 
+### Lesson 1 todo
+
+1. Create a server with express & respond index.html on a GET request to '/'
+2. Respond json data on a GET to '/data'
+
---
+++ server.js
@@ -0,0 +1,7 @@
+/*
+ * TODO: create a basic server with express
+ * respond index.html on a get request to '/'
+ * respond jsonData on a get request to '/data'
+ */
+
+ var jsonData = {count: 12, message: 'Hello World'}
